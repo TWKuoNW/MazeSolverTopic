002e299c3f098b24ba6b9dd37b97a0b79a24fd7a
TWKuoNW <105274665+TWKuoNW@users.noreply.github.com>
2023-05-27 13:06:01 +0800

    Update Run.java

--- 迷宮解決器/Run.java
+++ 迷宮解決器/Run.java
@@ -1,9 +1,17 @@
 package Test;
 
 public class Run {
-    public static void main(String args[]){
-        CreatMaze1 m = new CreatMaze1(11, 11);
-        MazeSolver1 solver = new MazeSolver1(m.getIntMaze());
-        solver.printSolution();
+
+    public static void main(String args[]) {
+        RecordMaze1 rm = new RecordMaze1(100);
+        for (int i = 1; i <= 100; i++) {
+            CreatMaze1 m = new CreatMaze1(11, 11);
+            long startTime = System.nanoTime();
+            MazeSolver1 solver = new MazeSolver1(m.getIntMaze());
+            long endTime = System.nanoTime();
+            solver.printSolution();
+            rm.addMaze(m.getMaze(), (endTime - startTime));
+        }
+
     }
 }
